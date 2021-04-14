2ce6defbe859a43bf785e0de222c62db7310fb85
Ronak Patel <36149464+patelronakp@users.noreply.github.com>
2021-04-14 10:30:02 +0530

    change file and folder name

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './global.scss';
-import Header from './components/header/Header';
-import SideDrawer from './components/SideDrawer/SideDrawer';
-import Backdrop from './components/Backdrop/Backdrop';
+import Header from './components/header';
+import SideDrawer from './components/sideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop';
 import { useState } from 'react';
-import Home from './screens/homeScreen/home'
+import Home from './screens/homeScreen'
+
 function App() {
   const [toggleDrawer, setToggleDrawer] = useState(false);
   const drawerToggleClickHandler = () => {
     setToggleDrawer(!toggleDrawer);
   }
+
   const backDropClickHandler = () => {
     setToggleDrawer(false);
   }
+
   let backdrop;
   if (toggleDrawer) {
     backdrop = <Backdrop backDropClickHandler={backDropClickHandler} />;
--- src/screens/homeScreen/index.jsx
+++ src/screens/homeScreen/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Slider from "../../components/slider";
 import FeatureCategory from "../../components/featureCategory";
-import Products from "../../components/products/products";
+import Products from "../../components/products";
 const Home = () => {
   return (
     <>
