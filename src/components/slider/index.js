6a5ed9f3c016a8901c01b1647091a13f0cd5dfa2
Ronak Patel <36149464+patelronakp@users.noreply.github.com>
2021-05-04 23:04:11 +0530

    remove unused var

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,35 +1,13 @@
 import React from 'react';
 import './styles.scss';
-import vectorCollection from '../../assets/images/vector-collections.svg';
 import banner1 from '../../assets/homeBanner/Banner1.png'
 import banner2 from '../../assets/homeBanner/banner2.png'
 import banner3 from '../../assets/homeBanner/banner3.png'
 import SlideShow from '../slideShow';
 
-const sliders = [
-    {
-        id: 1,
-        img: `https://picsum.photos/1040/800?random=${Math.random()}`,
-        title: 'Meet New Fashion Week'
-    },
-    // {
-    //     id: 2,
-    //     img: `https://picsum.photos/1040/800?random=${Math.random()}`,
-    //     title: 'Meet New Fashion Week'
-    // },
-];
 
 
-const Slider = props => {
-
-    const sliderContent = sliders.map(({ id, img, title }) => (
-        <div key={id} className="slide">
-            <img src={banner1} alt={title} />
-            {/* <img src={vectorCollection} alt="test" /> */}
-        </div>
-    ));
-
-
+const Slider = () => {
     return (
         <div className="bannerSlider">
             <div className="slide">
